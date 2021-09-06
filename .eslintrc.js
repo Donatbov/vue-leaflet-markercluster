4a7f4a84972976f5c9aa8225f5afdac9a9014295
Vivien Donat-Bouillud <vivien.donat-bouillud@pentila.com>
2021-09-06 10:02:23 +0200

    Add map example in App component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['@vue/standard'],
+  extends: ['plugin:vue/vue3-recommended', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LMap
+      class="map"
+      :zoom="zoom"
+      :max-zoom="19"
+      :use-global-leaflet="true"
+      :center="center"
+      :options="mapOptions"
+    >
+      <LTileLayer
+        :url="url"
+        :attribution="attribution"
+      />
+      <LMarker
+        v-for="object in objectList"
+        :key="object.id"
+        :lat-lng="object.latlng"
+      />
+    </LMap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'leaflet/dist/leaflet.css'
+import { latLng } from 'leaflet'
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      zoom: 8,
+      center: latLng(46.35312518717163, 6.784678858852737),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      objectList: [
+        {
+          latlng: latLng(46.7934204, 7.1568772)
+        },
+        {
+          latlng: latLng(47.3625149, 7.3499802)
+        },
+        {
+          latlng: latLng(46.3179423, 7.9809608)
+        },
+        {
+          latlng: latLng(46.9976485, 6.9365306)
+        }
+      ]
+    }
   }
 }
 </script>
@@ -23,6 +68,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 95vh;
+  width: 95vw;
 }
 </style>
